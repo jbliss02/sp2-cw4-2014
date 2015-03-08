4961aa9460dd40814a1e0ae472fe0ffd33699937
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2015-03-08 20:00:24 +0000

    started to place ships randomly in ocean only one ship so far

--- src/battleships/Ocean.java
+++ src/battleships/Ocean.java
@@ -3,6 +3,8 @@
  */
 package battleships;
 
+import java.util.Random;
+
 /**
  * @author Jbliss02
  * This contains a 10x10 array of Ships, representing the “ocean”, and some
@@ -10,7 +12,7 @@ package battleships;
  */
 public class Ocean implements IOcean {
 
-	public Ship[][] ships = new Ship[10][10];
+	private Ship[][] ships = new Ship[10][10];
 	private int shotsFired; //The total number of shots fired by the user
 	private int hitCount; //The number of times a shot hit a ship. If the user shoots the same part of a ship more than once, every hit is counted
 	private int shipsSunk; //The number of ships sunk
@@ -23,6 +25,7 @@ public class Ocean implements IOcean {
 		shotsFired = 0;
 		hitCount = 0;
 		shipsSunk = 0;
+		createEmptyOcean();
 	}
 	
 	
@@ -31,7 +34,14 @@ public class Ocean implements IOcean {
 	 */
 	@Override
 	public boolean isOccupied(int row, int column) {
-		return false;
+		
+		if(ships[row][column].getClass().getSimpleName().equals("EmptySea")){
+			return false;
+		}
+		else{
+			return true;
+		}
+
 	}
 	
 	
@@ -99,7 +109,111 @@ public class Ocean implements IOcean {
 	@Override
 	public void placeAllShipsRandomly() {
 		
-	}
+		Random random = new Random();
+		
+		//start with one ship
+		IShip battleship = new Battleship();
+		
+		//keep randomly placing the ship until it fits somewhere
+		boolean shipPlaced = false;
+		do
+		{
+			//randomly place horizontally or vertically
+			battleship.setHorizontal(random.nextBoolean());
+			
+			battleship.setHorizontal(true);
+			
+			//get a random start row and column
+			int startRow = random.nextInt(ships.length);
+			int startCol = random.nextInt(ships[0].length);
+					
+			//set a direction to go in from the initial row		
+			if(battleship.isHorizontal()){
+				
+				//go left or right
+				boolean right = random.nextBoolean();
+				if(right && startCol + battleship.getLength() > ships[0].length){right = false;}//go left if no room right
+				if(!right && startCol - battleship.getLength() < 0){right = true;}//go right if no room left
+				
+				//check if there is room
+				if(roomHorizontally(startRow, startCol, battleship.getLength(), right)){
+					System.out.println("yes");
+					shipPlaced = true;
+				}
+				else{
+					System.out.println("no");
+				}
+					
+			}
+			else {
+				boolean up = random.nextBoolean(); 
+				if(up && startRow - battleship.getLength() < 0){up = false;} //go down if no room to go up
+				if(!up && startRow + battleship.getLength() > ships.length){up = true;} //go up if no room down
+			}
+		
+		
+		}while(!shipPlaced);
+			
+		System.out.print("ship placed");
+		
+	}//placeAllShipsRandomly()
+	
+	/**
+	 * Checks whether a ship can be placed horizontally given a start row, start column, ship length and direction of travel
+	 * @param startRow
+	 * @param startCol
+	 * @param length
+	 * @param moveRight
+	 * @return
+	 */
+	private boolean roomHorizontally(int startRow, int startCol, int length, boolean moveRight){
+		
+		if(moveRight){
+			
+			for(int i = startCol; i < startCol + length + 1; i++){
+				
+				if(isOccupied(startRow, i)){
+					return false;
+				}
+				
+			}
+			
+			return true;
+		}
+		else{ //moving left
+			
+			for(int i = startCol; i > startCol - length - 1; i--){
+				
+				if(isOccupied(startRow, i)){
+					return false;
+				}
+			}
+			
+			return true;
+			
+		}//whether we are moving left or right
+		
+	}//roomHorizontally()
+	
+	
+
+	/**
+	 * Populates the array with EmptyOcean objects,
+	 * used at the start of the game to set the ocean to "empty"
+	 */
+	private void createEmptyOcean(){
+		
+		for(int i = 0; i < ships.length; i++){
+			
+			for(int n = 0; n < ships[i].length; n++){
+				
+				ships[i][n] = new EmptySea();
+				
+			}//n
+			
+		}//i
+		
+	}//createEmptyOcean()
 	
 	
 }//Ocean class ends
--- src/battleships/OceanTest.java
+++ src/battleships/OceanTest.java
@@ -4,13 +4,29 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class OceanTest {
 
-	@Test
+	
 	public void test() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void testIsOccupied_allempty(){
+		
+		for(int row = 0; row < 10; row++){
+			for(int col = 0; col < 10; col++){
+				assertEquals("Empty Test", false, new Ocean().isOccupied(row, col));
+			}
+		}
 
-	
+	}//testIsOccupied_allempty
+
+	@Test
+	public void placeShipsByRandom(){
+		
+		new Ocean().placeAllShipsRandomly();
+		
+	}
 }
