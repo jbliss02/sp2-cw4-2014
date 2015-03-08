6b36621c5fc6258f901bba0269ebfd2b2dbea5b3
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2015-03-08 19:01:19 +0000

    started testing ship class

--- src/battleships/ShipTest.java
+++ src/battleships/ShipTest.java
@@ -6,9 +6,33 @@ import org.junit.Test;
 
 public class ShipTest {
 
-	@Test
+
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test 
+	public void testType(){	
+		assertEquals("Battleship", "Battleship", new Battleship().getShipType());
+		assertEquals("Cruiser", "Cruiser", new Cruiser().getShipType());
+		assertEquals("Destroyer", "Destroyer", new Destroyer().getShipType());
+		assertEquals("Submarine", "Submarine", new Submarine().getShipType());
+	}
+	
+	@Test
+	public void testLength(){
+		assertEquals("Battleship", 4, new Battleship().getLength());
+		assertEquals("Cruiser", 3, new Cruiser().getLength());
+		assertEquals("Destroyer", 2, new Destroyer().getLength());
+		assertEquals("Submarine", 1, new Submarine().getLength());		
+	}
+	
+
+	public void gettersSetters(){
+		
+		
+		
+	}
+	
 
 }
